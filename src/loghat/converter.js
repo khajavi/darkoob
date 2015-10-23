8669db165eac7277f8948f03b4eaf5be2b2c32f9
Milad Khajvi <khajavi@gmail.com>
2015-10-23 16:20:51 +0330

    parse footnotes in more detail

--- src/loghat/converter.js
+++ src/loghat/converter.js
@@ -29,6 +29,55 @@ function addSeparator(doc) {
   return doc;
 }
 
+function Ref() {
+  this.sooreh = null;
+  this.ayat = [];
+  this.jeld = null;
+  this.pages = [];
+}
+
+function parseFootnote2(footnote) {
+  var re = /([\u0600-\u06FF\uFB8A\u067E\u0686\u06AF]+|\w+)،?( | )? ?(aye|آیه|raye|رآیه)( | )? ? ?(\d+)( | )? ?(r|ر)?\((j|ج)( | )? ?(\d+)،( | )?( | )?(s|ص|safhe|صفحه)?( | )?( | )?(\d+)\).?/g;
+  var res = re.exec(footnote);
+  var note = new Ref();
+
+  if (res !== null) {
+    note.sooreh = res[1].slice(0, res[1].length - 1);
+    note.ayat.push(res[5]);
+    note.jeld = res[10];
+    note.pages.push(res[16]);
+  }
+  if (res === null) {
+    var re1 = /([\u0600-\u06FF\uFB8A\u067E\u0686\u06AF]+|\w+)،?( | )?( | )?(ayat|آیات|aye|آیه) ?(\d+)( ?(،|v|و) ? ?(\d+))?( ?(،|v|و) ?(\d+))?( | )?( | )?\((j|ج)( | )?( | )?(\d+)،( | )?( | )?(safahat|صفحات|s|ص)( | )?( | )?(\d+)(،( | )?( | )?(\d+)(، ?(\d+))?(، ? ?(\d+))?)?\).?/g;
+    var res1 = re1.exec(footnote);
+    if (res1 !== null) {
+      note.sooreh = res1[1].slice(0, res1[1].length - 1);
+      note.ayat.push(res1[5]);
+      note.ayat.push(res1[8]);
+      note.jeld = res1[17];
+      note.pages.push(res1[23]);
+      note.pages.push(res1[27]);
+    }
+
+    if (res1 === null) {
+      var re2 = /([\u0600-\u06FF\uFB8A\u067E\u0686\u06AF]+|\w+)، ?(aye|آیه) ? ?(\d+)/g;
+      var res2 = re2.exec(footnote);
+      if (res2 !== null) {
+        note.sooreh = res2[1].slice(0, res2[1].length - 1);
+        note.ayat.push(res2[3]);
+      }
+    }
+  }
+
+  if (res2 === null) {
+    //console.log(footnote);
+    return null;
+  } else {
+    return note;
+  }
+}
+
+
 function parseFootnote(doc) {
   $ = cheerio.load(doc, {
     decodeEntities: false
@@ -44,9 +93,21 @@ function parseFootnote(doc) {
 
     var note = footnote.match(regex);
 
+
+    var entries = note[2].split(";");
+    entries = entries.map(function (ent) {
+      ent = ent.replace(/<br>/g, '');
+      return ent.trim();
+    });
+
+    entries = entries.map(function (e) {
+      return parseFootnote2(e);
+    });
+
+
     list.push({
       index: note[1],
-      note: note[2]
+      note: entries
     });
   });
   return list;
@@ -75,14 +136,14 @@ function parseDoc(doc, footnotes) {
     }
 
     var footnotesIdx = [];
-var rgx = /\((\d+)\)/g;
+    var rgx = /\((\d+)\)/g;
     var match;
-    while( (match = rgx.exec(description)) != null) {
+    while ((match = rgx.exec(description)) != null) {
       footnotesIdx.push(match[1]);
     }
 
     var notes = _.map(footnotesIdx, function (id) {
-     return _.find(footnotes, function (footnote) {
+      return _.find(footnotes, function (footnote) {
         return footnote.index == id;
       })
     });
